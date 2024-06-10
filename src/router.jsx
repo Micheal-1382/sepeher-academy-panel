06ba412e3ad3678e2e56a1bb7789640507db8bad
micheal aghaei <aghaeimicheal@gmail.com>
2024-06-10 19:56:30 +0330

    create rtl panel

---
+++ src/router.jsx
@@ -0,0 +1,66 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login, { loginAction } from "./features/Identity/components/login/login";
+import Register from "./features/Identity/components/register/register";
+import IdentityLayout from "./layouts/identity-layout";
+import { registerAction } from "./features/Identity/components/register/register";
+import MainLayout from "./layouts/mainLayout/main-layout";
+import Courses, { coursesLoader } from "./pages/courses";
+import CourseCategories, { categoriesLoader } from "./pages/course-categories";
+import CourseDetails, {
+  courseDetailsLoader,
+} from "./features/courses/components/course-details";
+import { CategoryProvider } from "./features/categories/category-context";
+import NotFound from "./pages/not-found";
+import UnhandledException from "./pages/unhandled-exception";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <UnhandledException />,
+    children: [
+      {
+        index: true,
+        element: <Courses />,
+        loader: coursesLoader,
+      },
+      {
+        path: "courses/:id",
+        element: <CourseDetails />,
+        loader: courseDetailsLoader,
+      },
+      {
+        path: "course-categories",
+        element: (
+          <CategoryProvider>
+            <CourseCategories />
+          </CategoryProvider>
+        ),
+        loader: categoriesLoader,
+      },
+    ],
+  },
+  {
+    element: <IdentityLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction,
+        errorElement: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+        action: registerAction,
+        errorElement: <Register />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
