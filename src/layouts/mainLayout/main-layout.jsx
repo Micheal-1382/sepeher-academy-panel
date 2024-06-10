80e9a031e0930e4da587a4def38abdf53acbcc66
micheal aghaei <aghaeimicheal@gmail.com>
2024-06-10 21:53:08 +0330

    add tailwind

--- src/layouts/mainLayout/main-layout.jsx
+++ src/layouts/mainLayout/main-layout.jsx
@@ -26,7 +26,7 @@ const MainLayout = () => {
             <div className="row">
               <div className="col-12">
                 <p className="mb-0">
-                  © 2023 - <a className="text-muted">{t("classbon")}</a>
+                طراحی شده با ♥️ توسط تیم Sleepy Guys
                 </p>
               </div>
             </div>
